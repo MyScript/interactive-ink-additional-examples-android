bb676244a0c7198d14a7379b860651cdd2124d72
Olivier Patry <olivier.patry@myscript.com>
2023-04-29 18:03:02 +0200

    [iink][examples] Align iink Android Offscreen Interactivity Demo style with regular Demo

--- src/main/java/com/myscript/iink/demo/inksample/ui/MainActivity.kt
+++ src/main/java/com/myscript/iink/demo/inksample/ui/MainActivity.kt
@@ -10,13 +10,8 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
 import android.webkit.WebSettings
-import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.activity.viewModels
@@ -28,144 +23,124 @@ import androidx.core.graphics.red
 import androidx.core.view.isVisible
 import com.myscript.iink.demo.ink.InkView
 import com.myscript.iink.demo.ink.InkView.Brush
-import com.myscript.iink.demo.ink.InkView.DynamicPaintHandler
 import com.myscript.iink.demo.ink.InputManager
-import com.myscript.iink.offscreen.demo.R
+import com.myscript.iink.offscreen.demo.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var inkView: InkView
-    private lateinit var webView: WebView
-    private lateinit var fancySwitch: SwitchCompat
-    private lateinit var seekBar: SeekBar
-    private lateinit var circleView: ImageView
+    private val binding by lazy { MainActivityBinding.inflate(layoutInflater) }
 
     private val inkViewModel: InkViewModel by viewModels { InkViewModel.Factory }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        inkView = findViewById(R.id.inkView)
-        webView = findViewById(R.id.webView)
-        fancySwitch = findViewById(R.id.fancySwitch)
-        seekBar = findViewById(R.id.seekBar)
-        circleView = findViewById(R.id.circleView)
+        setContentView(binding.root)
 
-        setupClickListeners()
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            settings.apply {
+                javaScriptEnabled = true
+                cacheMode = WebSettings.LOAD_NO_CACHE
+            }
+            loadUrl("https://en.wikipedia.org/wiki/Special:Random")
+        }
 
-        webView.webViewClient = WebViewClient()
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
+        setStrokeWidth(binding.inkWidthSeekbar.progress)
 
-        webView.loadUrl("https://en.wikipedia.org/wiki/Special:Random")
-
-        seekBar.setOnSeekBarChangeListener(
-            object : OnSeekBarChangeListener {
-                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                    setStrokeWidth()
-                }
-
-                override fun onStartTrackingTouch(seekBar: SeekBar) {}
-                override fun onStopTrackingTouch(seekBar: SeekBar) {}
-            },
-        )
-
-        setStrokeWidth()
-
-        inkViewModel.strokes.observe(this, inkView::drawStrokes)
+        inkViewModel.strokes.observe(this, binding.inkView::drawStrokes)
     }
 
     override fun onStart() {
         super.onStart()
 
-        inkView.strokesListener = StrokesListener()
-    }
+        with(binding) {
+            inkView.strokesListener = StrokesListener()
+            clearInkBtn.setOnClickListener { inkViewModel.clearInk() }
+            saveInkBtn.setOnClickListener { inkViewModel.saveInk() }
+            loadInkBtn.setOnClickListener { inkViewModel.loadInk() }
+            inkPressureSwitch.setOnClickListener {
+                inkView.dynamicPaintHandler = when {
+                    (it as SwitchCompat).isChecked -> FancyPaintHandler()
+                    else -> null
+                }
+            }
 
-    override fun onStop() {
-        super.onStop()
+            webViewSwitch.setOnClickListener {
+                webView.isVisible = (it as SwitchCompat).isChecked
+            }
 
-        inkView.strokesListener = null
-    }
+            inkWidthSeekbar.setOnSeekBarChangeListener(
+                    object : OnSeekBarChangeListener {
+                        override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                            setStrokeWidth(progress)
+                        }
 
-    fun setStrokeWidth() {
-        val layoutParams: ViewGroup.LayoutParams = circleView.layoutParams
-        val factor = resources.displayMetrics.density
-        layoutParams.width = (seekBar.progress * factor).toInt()
-        layoutParams.height = (seekBar.progress * factor).toInt()
-        circleView.layoutParams = layoutParams
-
-        if (inkView.pressureEnabled) {
-            inkView.strokeWidth = seekBar.progress / STROKE_MAX_MIN_RATIO
-            inkView.strokeWidthMax = seekBar.progress.toFloat()
-        } else {
-            inkView.strokeWidth = seekBar.progress.toFloat()
-            inkView.strokeWidthMax = seekBar.progress.toFloat()
+                        override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
+                        override fun onStopTrackingTouch(seekBar: SeekBar) = Unit
+                    }
+            )
         }
     }
 
-    private fun setupClickListeners() {
-        findViewById<Button>(R.id.btnClear)?.setOnClickListener(::clickClear)
-        findViewById<Button>(R.id.btnSave)?.setOnClickListener(::saveInk)
-        findViewById<Button>(R.id.btnLoad)?.setOnClickListener(::loadInk)
-        fancySwitch.setOnClickListener(::fancySwitchChanged)
-        findViewById<SwitchCompat>(R.id.webSwitch)?.setOnClickListener(::webSwitchChanged)
-        findViewById<ImageView>(R.id.imageCopy)?.setOnClickListener(::copyImage)
+    override fun onStop() {
+        with(binding) {
+            inkView.strokesListener = null
+            clearInkBtn.setOnClickListener(null)
+            saveInkBtn.setOnClickListener(null)
+            loadInkBtn.setOnClickListener(null)
+            inkPressureSwitch.setOnClickListener(null)
+            webViewSwitch.setOnClickListener(null)
+            inkWidthSeekbar.setOnSeekBarChangeListener(null)
+        }
+
+        super.onStop()
     }
 
-    private fun clickClear(@Suppress("UNUSED_PARAMETER") view: View) {
-        inkViewModel.clearInk()
-    }
+    private fun setStrokeWidth(ratio: Int) {
+        with(binding.inkWidthFeedback) {
+            layoutParams = layoutParams.apply {
+                val density = resources.displayMetrics.density
+                val inkWidth = (ratio * density).toInt()
+                width = inkWidth
+                height = inkWidth
+            }
+        }
 
-    private fun copyImage(view: View) {
-        val image = view as ImageView
-        image.setImageBitmap(inkView.saveBitmap())
-    }
-
-    private fun saveInk(@Suppress("UNUSED_PARAMETER") view: View) {
-        inkViewModel.saveInk()
-    }
-
-    private fun loadInk(@Suppress("UNUSED_PARAMETER") view: View) {
-        inkViewModel.loadInk()
-    }
-
-    private fun fancySwitchChanged(view: View) {
-        val switch = view as SwitchCompat
-        if (switch.isChecked) {
-            inkView.dynamicPaintHandler = FancyPaintHandler()
-        } else {
-            inkView.dynamicPaintHandler = null
+        with(binding.inkView) {
+            strokeWidth = when {
+                pressureEnabled -> ratio / STROKE_MAX_MIN_RATIO
+                else -> ratio.toFloat()
+            }
+            strokeWidthMax = ratio.toFloat()
         }
     }
 
     /**
      * Renders the ink with transparency linked to the pressure on the pen.
      */
-    inner class FancyPaintHandler : DynamicPaintHandler {
+    inner class FancyPaintHandler : InkView.DynamicPaintHandler {
         override fun generatePaintFromPenInfo(penInfo: InputManager.PenInfo): Paint {
-            val paint = Paint()
-            val alpha = penInfo.pressure * 255
-
-            paint.color = Color.argb(
-                alpha.toInt(),
-                inkView.color.red,
-                inkView.color.green,
-                inkView.color.blue
-            )
-            paint.isAntiAlias = true
-            // Set stroke width based on display density.
-            paint.strokeWidth = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                penInfo.pressure * (inkView.strokeWidthMax - inkView.strokeWidth) + inkView.strokeWidth,
-                resources.displayMetrics
-            )
-            paint.style = Paint.Style.STROKE
-            paint.strokeJoin = Paint.Join.ROUND
-            paint.strokeCap = Paint.Cap.ROUND
-
-            return paint
+            return Paint().apply {
+                val alpha = penInfo.pressure * 255
+                val inkViewColor = binding.inkView.color
+                color = Color.argb(
+                        alpha.toInt(),
+                        inkViewColor.red,
+                        inkViewColor.green,
+                        inkViewColor.blue
+                )
+                isAntiAlias = true
+                // Set stroke width based on display density.
+                strokeWidth = TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        penInfo.pressure * (binding.inkView.strokeWidthMax - binding.inkView.strokeWidth) + binding.inkView.strokeWidth,
+                        resources.displayMetrics
+                )
+                style = Paint.Style.STROKE
+                strokeJoin = Paint.Join.ROUND
+                strokeCap = Paint.Cap.ROUND
+            }
         }
     }
 
@@ -176,11 +151,6 @@ class MainActivity : AppCompatActivity() {
         override fun onStrokeAdded(brush: Brush) {
             inkViewModel.addStroke(brush)
         }
-    }
-
-    private fun webSwitchChanged(view: View) {
-        val switch = view as SwitchCompat
-        this.webView.isVisible = switch.isChecked
     }
 
     companion object {
