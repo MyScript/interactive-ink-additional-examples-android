cb2ef812f48db1ff0dc090794ee194de7d464aa1
Thibault Guegan <thibault.guegan@myscript.com>
2023-05-23 17:05:28 +0200

    [iink][examples] initialize Engine (MSIS-4769)

---
+++ src/main/java/com/myscript/iink/demo/inksample/InkApplication.kt
@@ -0,0 +1,30 @@
+package com.myscript.iink.demo.inksample
+
+import android.app.Application
+import com.myscript.certificate.MyCertificate
+import com.myscript.iink.Engine
+import java.io.File
+
+class InkApplication: Application() {
+
+    var engine: Engine? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        engine =  Engine.create(MyCertificate.getBytes()).apply {
+            configuration.let { conf ->
+                val confDir = "zip://${packageCodePath}!/assets/conf"
+                conf.setStringArray("configuration-manager.search-path", arrayOf(confDir))
+                val tempDir = File(cacheDir, "tmp")
+                conf.setString("content-package.temp-folder", tempDir.absolutePath)
+            }
+        }
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        engine?.close()
+        engine = null
+    }
+}
