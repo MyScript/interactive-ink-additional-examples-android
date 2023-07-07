189af282080c4d44af51d260cd8cb7813beb96af
Yuntaek Rim <yuntaek.rim@myscript.com>
2023-07-07 21:08:44 +0900

    update of additional samples to iink 2.1.1

--- java/samples/exercise-assessment-kt/src/main/java/com/myscript/iink/samples/assessment/MathGrammarK8DynamicRes.kt
+++ java/samples/exercise-assessment-kt/src/main/java/com/myscript/iink/samples/assessment/MathGrammarK8DynamicRes.kt
@@ -12,24 +12,24 @@ import java.io.IOException
 class MathGrammarK8DynamicRes
 {
     private val fileName = "math-grm-standardK8.res"
-    private val k8Grammar = """symbol = 0 1 2 3 4 5 6 7 8 9 + - / ÷ = . , % | ( ) : * x
-leftpar = (
-rightpar = )
-currency_symbol = $ R € ₹ £
-character ::= identity(symbol)
-            | identity(currency_symbol)
-fractionless ::= identity(character)
-               | fence (fractionless, leftpar, rightpar)
-               | hpair(fractionless, fractionless)
-fractionable ::= identity(character)
-               | fence (fractionable, leftpar, rightpar)
-               | hpair(fractionable, fractionable)
-               | fraction(fractionless, fractionless)
-expression ::= identity(character)
-             | fence (expression, leftpar, rightpar)
-             | hpair(expression, expression)
-             | fraction(fractionable, fractionable)
-start(expression)"""
+    private val k8Grammar = "symbol = 0 1 2 3 4 5 6 7 8 9 + - / ÷ = . , % | ( ) : * x\n" +
+        "leftpar = (\n" +
+        "rightpar = )\n" +
+        "currency_symbol = \$ R € ₹ £\n" +
+        "character ::= identity(symbol)\n" +
+        "            | identity(currency_symbol)\n" +
+        "fractionless ::= identity(character)\n" +
+        "               | fence (fractionless, leftpar, rightpar)\n" +
+        "               | hpair(fractionless, fractionless)\n" +
+        "fractionable ::= identity(character)\n" +
+        "               | fence (fractionable, leftpar, rightpar)\n" +
+        "               | hpair(fractionable, fractionable)\n" +
+        "               | fraction(fractionless, fractionless)\n" +
+        "expression ::= identity(character)\n" +
+        "             | fence (expression, leftpar, rightpar)\n" +
+        "             | hpair(expression, expression)\n" +
+        "             | fraction(fractionable, fractionable)\n" +
+        "start(expression)"
 
     @Throws(IllegalArgumentException::class, RuntimeException::class, IOException::class)
     fun build(eng : Engine, filePath : String) {
